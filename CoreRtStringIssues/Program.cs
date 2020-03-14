4022ace8b1b01d3cea148b13d8ba9529a0478169
Andrii Kurdiumov <kant2002@gmail.com>
2020-03-14 18:34:36 +0600

    Add project files.

---
+++ CoreRtStringIssues/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreRtStringIssuesCustomRuntime
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Hello World!"[3]);
+        }
+    }
+}
